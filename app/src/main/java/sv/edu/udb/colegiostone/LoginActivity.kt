4e25aa2452304eb81927bde6d19896ac4a05baad
YensyC <ale_barahona93@hotmail.com>
2025-03-29 00:50:51 -0600

    Validacion usuario

--- app/src/main/java/sv/edu/udb/colegiostone/LoginActivity.kt
+++ app/src/main/java/sv/edu/udb/colegiostone/LoginActivity.kt
@@ -5,12 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var buttonRegistrarme: Button
+    private lateinit var buttonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        buttonRegistrarme = findViewById(R.id.btnRegistroNuevo)
+        buttonRegistrarme.setOnClickListener {
+
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonLogin = findViewById(R.id.btnLogin)
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,3 +40,4 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
+
